ed057c623f4e96976d1fe66a8c4711758c991259
Jeanne Pietropaoli <j.pietropaoli@yahoo.fr>
2022-05-31 01:29:04 +0200

    Refacto js file

--- essai.js
+++ essai.js
@@ -22,10 +22,6 @@ function Book(bookTitle, bookAuthor, bookPages, bookStatus){
     this.status = bookStatus;
 }
 
-Book.prototype.info = function(){
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.status}.`;
-}
-
 Book.prototype.titleInfo = function(){
     bookToDisplay.appendChild(document.createElement('p')).textContent = `${this.title}`;
 }
@@ -38,96 +34,152 @@ function addBookToLibrary() {
     books.push(new Book(titleInput, authorInput, pagesInput , statusInput));
 }
 
-function displayNewBook(arrOfBooks){
-    let bookToDisplay = document.createElement('div');
-    bookToDisplay.setAttribute('data-index', arrOfBooks.length-1);
+Book.prototype.createBookCard = function() {
+    const bookToDisplay = document.createElement('div');
+    bookToDisplay.setAttribute('data-index', books.length-1);
+    return bookToDisplay;
+}
 
-    let deleteBook = document.createElement('span');
+Book.prototype.titleInfos = function(){
+    const titleDisplay = document.createElement('p');
+    titleDisplay.textContent = (books[books.length-1].title).toUpperCase();
+    return titleDisplay;
+}
+
+Book.prototype.authorInfos = function() {
+    const authorDisplay = document.createElement('p');
+    authorDisplay.textContent = `By : ${books[books.length-1].author}`;
+    return authorDisplay;
+}
+
+Book.prototype.pagesInfos = function() {
+    const pagesDisplay = document.createElement('p');
+    pagesDisplay.textContent = `Number of pages : ${books[books.length-1].pages}`;
+    return pagesDisplay;
+}
+
+Book.prototype.createDeleteBookBtn = function() {
+    const deleteBook = document.createElement('span');
     deleteBook.textContent = 'x';
     deleteBook.style.cursor = 'pointer';
     deleteBook.classList.add('deleteBook');
-    
-    let titleDisplay = document.createElement('p');
-    titleDisplay.textContent = (arrOfBooks[arrOfBooks.length-1].title).toUpperCase();
+    return deleteBook;   
+}
 
-    let authorDisplay = document.createElement('p');
-    authorDisplay.textContent = `By : ${arrOfBooks[arrOfBooks.length-1].author}`;
-
-    let pagesDisplay = document.createElement('p');
-    pagesDisplay.textContent = `Number of pages : ${arrOfBooks[arrOfBooks.length-1].pages}`;
-
-    let statusDisplay = document.createElement('div');
-    let checkbox = document.createElement('input');
+Book.prototype.createCheckbox = function() {
+    const checkbox = document.createElement('input');
     checkbox.setAttribute('type', 'checkbox');
-    let slider = document.createElement('div');
+    return checkbox;
+}
+
+Book.prototype.createSlider = function() {
+    const slider = document.createElement('div');
     slider.classList.add('slider');
     slider.style.backgroundColor = 'white';
-    slider.setAttribute('data-index', arrOfBooks.length-1);
-    let statusDis = document.createElement('span');
+    slider.setAttribute('data-index', books.length-1);
+    return slider;
+}
+
+Book.prototype.statusInfos = function() {
+    const statusDis = document.createElement('span');
     statusDis.classList.add('test');
-    statusDis.textContent = arrOfBooks[arrOfBooks.length-1].status;
-    
-    let sliderPiece = document.createElement('span');
+    statusDis.textContent = books[books.length-1].status;
+    return statusDis;
+}
+
+Book.prototype.appendBookElements = function (statusDisplay, checkbox, statusDis, deleteBook, titleDisplay, authorDisplay, pagesDisplay, bookToDisplay, slider, sliderPiece) {
     statusDisplay.append(checkbox, statusDis, slider);
     slider.appendChild(sliderPiece);
+    bookToDisplay.append(deleteBook, titleDisplay, authorDisplay, pagesDisplay, statusDisplay);
+    CONTAINER.appendChild(bookToDisplay);
+}
 
-    if (arrOfBooks[arrOfBooks.length-1].status === 'read'){
+Book.prototype.defineSliderColor = function(sliderPiece) {
+    if (books[books.length-1].status === 'read'){
         sliderPiece.style.marginLeft = 0;
         sliderPiece.style.backgroundColor = '#E62100';
     } else {
         sliderPiece.style.marginLeft = '31px';
         sliderPiece.style.backgroundColor = '#15454C';
     } 
+}
 
-    bookToDisplay.append(deleteBook, titleDisplay, authorDisplay, pagesDisplay, statusDisplay);
-    CONTAINER.appendChild(bookToDisplay);
-
+Book.prototype.deleteABook = function(deleteBook){
     deleteBook.addEventListener('click', (e) => {
         books.splice(e.target.parentElement.getAttribute('data-index'),1);
         CONTAINER.removeChild(e.target.parentElement);
     })
+}
 
+Book.prototype.switchStatus = function(slider, sliderPiece) {
     slider.addEventListener('click', ()=> {
-        console.log(slider.getAttribute('data-index'));
         slider.previousElementSibling.textContent === 'read' ? slider.previousElementSibling.textContent = 'not read yet' :
-        slider.previousElementSibling.textContent = 'read';
-        arrOfBooks[slider.getAttribute('data-index')].status = slider.previousElementSibling.textContent;
+            slider.previousElementSibling.textContent = 'read';
+        books[slider.getAttribute('data-index')].status = slider.previousElementSibling.textContent;
         sliderPiece.style.marginLeft === '31px' ? sliderPiece.style.marginLeft = 0 : sliderPiece.style.marginLeft = '31px';
-        console.log(sliderPiece);
         sliderPiece.style.marginLeft === '31px' ? sliderPiece.style.backgroundColor = '#15454C' : sliderPiece.style.backgroundColor = '#E62100';
     })
+}
+
+function displayNewBook(){
+    const bookToDisplay = books[books.length-1].createBookCard();
+    const titleDisplay = books[books.length-1].titleInfos();
+    const authorDisplay = books[books.length-1].authorInfos();
+    const pagesDisplay = books[books.length-1].pagesInfos();
+    const deleteBook = books[books.length-1].createDeleteBookBtn();
+
+    const statusDisplay = document.createElement('div');
+    const checkbox = books[books.length-1].createCheckbox();
+    const slider = books[books.length-1].createSlider();
+    const statusDis = books[books.length-1].statusInfos();
+    const sliderPiece = document.createElement('span');
+
+    books[books.length-1].appendBookElements(statusDisplay, checkbox, statusDis, deleteBook, titleDisplay, authorDisplay, pagesDisplay, bookToDisplay, slider, sliderPiece);
+    books[books.length-1].defineSliderColor(sliderPiece);
+    
+    books[books.length-1].deleteABook(deleteBook);
+    books[books.length-1].switchStatus(slider, sliderPiece);
 }
 
 function isOneInputInvalid(){
     return Array.from(FORM_INPUTS).some(input => input.checkValidity()===false);
 }
 
-ADDBOOK.addEventListener('click', function(){
-    MASK.style.opacity = 0.5;
+function formAppearence() {
+    MASK.style.opacity = 0.5; 
     MASK.classList.add('blur');
-    FORM_CONTAINER.classList.toggle('formAppearance');
+    FORM_CONTAINER.classList.add('formAppearance')
+}
+
+function formDisappearence() {
+    MASK.style.opacity = 1; 
+    MASK.classList.remove('blur');
+    FORM_CONTAINER.classList.remove('formAppearance');
+}
+
+function resetFormValues() {
+    FORM_CHECKBOX.value = 'on';
+    STATUS_DISPLAY.textContent = 'Book status : read';
+    FORM_INPUTS.forEach(input => input.value = "");
+    REQUIREDINPUTS.forEach(requiredInput => requiredInput.setAttribute('placeholder', ' '));
+    SLIDER_PIECE.style.marginLeft = 0;
+}
+
+ADDBOOK.addEventListener('click', function(){
+    formAppearence();
     FORM_CHECKBOX.value = 'read';
 })
 
 SUBMIT_BUTN.addEventListener('click', function() {
     if (!isOneInputInvalid()){
-        MASK.style.opacity = 1;
-        MASK.classList.remove('blur');
-        FORM_CONTAINER.classList.toggle('formAppearance');
+        formDisappearence();
         addBookToLibrary();
         displayNewBook(books);
-        FORM_CHECKBOX.value = 'on';
-        STATUS_DISPLAY.textContent = 'Book status : read';
-        FORM_INPUTS.forEach(input => input.value = "");
-        REQUIREDINPUTS.forEach(requiredInput => requiredInput.setAttribute('placeholder', ' '));
-        SLIDER_PIECE.style.marginLeft = 0;
+        resetFormValues();
 }
     else{
-        console.log(Array.from(FORM_INPUTS).filter(input => input.checkValidity()===false));
-        let arr = Array.from(FORM_INPUTS).filter(input => input.checkValidity()===false);
-        arr.forEach(item => {
-            item.setAttribute('placeholder', 'enter a valid value');
-    })
+        let invaliInputs = Array.from(FORM_INPUTS).filter(input => input.checkValidity()===false);
+        invaliInputs.forEach(item => item.setAttribute('placeholder', 'enter a valid value'))
     }
 })
 
@@ -138,27 +190,18 @@ REQUIREDINPUTS.forEach(requiredInput =>{
 })
 
 CANCEL_FORM.addEventListener('click', function(){
-    MASK.style.opacity = 1;
-    MASK.classList.remove('blur');
-    FORM_CONTAINER.classList.toggle('formAppearance');
+    formDisappearence();
     FORM_INPUTS.forEach(input => input.value = "");
-    FORM_CHECKBOX.value = 'on';
-    STATUS_DISPLAY.textContent = 'Book status : read';
-    REQUIREDINPUTS.forEach(requiredInput => requiredInput.setAttribute('placeholder', ' '));
-    SLIDER_PIECE.style.marginLeft = 0;
+    resetFormValues();
 } )
 
-GITHUBLINK.addEventListener('mouseover', function(){
-    GITHUBBTN.setAttribute('src', 'iconGitOrange.jpg');
-})
+GITHUBLINK.addEventListener('mouseover', () => GITHUBBTN.setAttribute('src', 'iconGitOrange.jpg'))
 
-GITHUBLINK.addEventListener('mouseout', function(){
-    GITHUBBTN.setAttribute('src', 'iconGit.jpg');
-})
+GITHUBLINK.addEventListener('mouseout', () => GITHUBBTN.setAttribute('src', 'iconGit.jpg'))
 
 SLIDER.addEventListener('click', () => {
     (STATUS_DISPLAY.textContent === 'Book status : read') ? STATUS_DISPLAY.textContent = 'Book status : not read yet' : 
-    STATUS_DISPLAY.textContent = 'Book status : read';
+        STATUS_DISPLAY.textContent = 'Book status : read';
     (FORM_CHECKBOX.value === 'read') ? FORM_CHECKBOX.value = 'not read yet' : FORM_CHECKBOX.value = 'read';
     SLIDER_PIECE.style.marginLeft === '31px' ? SLIDER_PIECE.style.marginLeft = 0 : SLIDER_PIECE.style.marginLeft = '31px';
     })
